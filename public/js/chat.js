af297d182ae8b648322f0a97081865b4291bf890
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-07 22:23:55 -0500

    auth chatjs

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,34 @@
+let user = null;
+let socket = null;
 
 
 
-const socket = io();
+const validarJWT = async () => {
+    const token = await localStorage.getItem('token')  || '';
+
+    if( token.length <= 10 ){
+        window.location = 'index.html';
+        throw new Error( 'No hay token en el servidor' );
+    }
+
+    const resp = await fetch( url, {
+        headers: { 'token': token }
+    } );
+
+    const { user: userdb, token: tokendb } = resp.json();
+    console.log( userdb, tokendb );
+
+
+}
+
+
+const main = async () => {
+    await validarJWT();
+}
+
+
+main();
+
+
+
+//const socket = io();
